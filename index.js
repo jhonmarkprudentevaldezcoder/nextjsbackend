7f91fecbfb1c72a1a5520ee7ef968991f1dc5d18
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-07 20:23:55 +0800

    DA

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 4000;
 
-// Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://test:kHJMUpuEUurItgpQ@cluster.4wnoari.mongodb.net/database",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-// Log a message when connected
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
 });
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
 });
+
+// Export the Express API
+module.exports = app;
