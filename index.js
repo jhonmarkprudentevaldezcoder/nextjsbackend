938eb8d8ee1c68469f611d8ef70ee378d259f096
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-07 20:46:40 +0800

    da

--- index.js
+++ index.js
@@ -1,61 +1,25 @@
-require("dotenv").config(); // environment variable
-
-// require packages
 const express = require("express");
 const mongoose = require("mongoose");
 
-// initialise express
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-//  mondodb connect
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-// create a schema
-const studentSchema = new mongoose.Schema({
-  roll_no: Number,
-  name: String,
-  year: Number,
-  subjects: [String],
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// create a model with studentSchema
-const Student = mongoose.model("Student", studentSchema);
-
-// Create a new document
-const stud = new Student({
-  roll_no: 1001,
-  name: "Madison Hyde",
-  year: 3,
-  subjects: ["DBMS", "OS", "Graph Theory", "Internet Programming"],
-});
-// Add the document to Collections
-stud.save().then(
-  () => console.log("One entry added"),
-  (err) => console.log(err)
-);
-// Save method can also be written as:
-// stud.save((err, result) => {
-//     if(err) console.log(err);
-//     else console.log("entry added");
-// });
-
-// get documents
-app.get("/", (req, res) => {
-  Student.find({}, (err, found) => {
-    if (!err) {
-      res.send(found);
-    } else {
-      console.log(err);
-      res.send("Some error occured!");
-    }
-  }).catch((err) => console.log("Error occured, " + err));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
-// Server listen
-app.listen(3000, () => console.log("Server listening to port 3000"));
+app.get("/api", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
+
+// Export the Express API
+module.exports = app;
