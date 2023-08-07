9da51a04b3d1eaf45d49709e506388e23587671b
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-07 20:07:28 +0800

    update

--- index.js
+++ index.js
@@ -1,61 +1,26 @@
-require("dotenv").config(); // environment variable
-
-// require packages
 const express = require("express");
 const mongoose = require("mongoose");
 
-// initialise express
 const app = express();
+const PORT = 4000;
 
-//  mondodb connect
-mongoose
-  .connect(process.env.MONGODB_URI, {
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://test:kHJMUpuEUurItgpQ@cluster.4wnoari.mongodb.net/database",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-// create a schema
-const studentSchema = new mongoose.Schema({
-  roll_no: Number,
-  name: String,
-  year: Number,
-  subjects: [String],
-});
-
-// create a model with studentSchema
-const Student = mongoose.model("Student", studentSchema);
-
-// Create a new document
-const stud = new Student({
-  roll_no: 1001,
-  name: "Madison Hyde",
-  year: 3,
-  subjects: ["DBMS", "OS", "Graph Theory", "Internet Programming"],
-});
-// Add the document to Collections
-stud.save().then(
-  () => console.log("One entry added"),
-  (err) => console.log(err)
+  }
 );
-// Save method can also be written as:
-// stud.save((err, result) => {
-//     if(err) console.log(err);
-//     else console.log("entry added");
-// });
 
-// get documents
-app.get("/", (req, res) => {
-  Student.find({}, (err, found) => {
-    if (!err) {
-      res.send(found);
-    } else {
-      console.log(err);
-      res.send("Some error occured!");
-    }
-  }).catch((err) => console.log("Error occured, " + err));
+// Log a message when connected
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
-// Server listen
-app.listen(3000, () => console.log("Server listening to port 3000"));
+// Define your routes and app.listen() here
+// ...
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
