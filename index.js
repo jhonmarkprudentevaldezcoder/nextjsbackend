6c2d8e2e5318ca99b8584e67fd2cd97b84aa37b8
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-07 20:51:18 +0800

    da

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
