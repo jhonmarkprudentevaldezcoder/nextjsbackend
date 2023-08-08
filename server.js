c7225b0b7b766d975e5b409eb880755c2aac87e0
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-08 22:38:24 +0800

    dada

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.delete("/products/:id", async (req, res) => {
 });
 
 //register user
-app.post("/register", async (req, res) => {
+app.post("/registers", async (req, res) => {
   // Check for a valid JWT token in the request headers
   const token = req.headers.authorization;
   if (!token || !token.startsWith("Bearer ")) {
@@ -114,6 +114,18 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//register
+app.post("/register", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+    console.log("user registered!");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
   }
 });
 
